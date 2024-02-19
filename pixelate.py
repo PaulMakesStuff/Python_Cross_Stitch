5fe206fd5449ad0d458a9e0036b45fa2e9bd96ee
francocontigo <ricardoaugustofranco@hotmail.com>
2024-02-19 10:57:36 -0300

    add: gooey interface

--- pixelate.py
+++ pixelate.py
@@ -18,6 +18,7 @@ import sys
 from PIL import Image
 from DMC import DMC
 from SVG import SVG
+from gooey import Gooey, GooeyParser
 
 def get_neighbours(pos, matrix):
     rows = len(matrix)
@@ -27,103 +28,93 @@ def get_neighbours(pos, matrix):
         for j in range(max(0, pos[1] - width), min(cols, pos[1] + width + 1)):
             if not (i == pos[0] and j == pos[1]):
                 yield matrix[i][j]
+
+@Gooey
+def main():
+    parser = GooeyParser(description="Generate Interface")
+
+    parser.add_argument("input_file_name", metavar="Input File Name", widget="FileChooser")
+    parser.add_argument("num_colours", metavar="Number of Colours", type=int, help="Number of colours to use in the pattern")
+    parser.add_argument("count", metavar="Stitch Count", type=int, help="Stitch count, number of stitches in x axis")
+
+    args = parser.parse_args()
+
+    input_file_name = args.input_file_name
+    num_colours = args.num_colours
+    count = args.count
+
+    col_sym = SVG(False, True, True)
+    blw_nsy = SVG(True, True, True)
+    col_nsy = SVG(False, False, False)
+    key = SVG(False, True, True)
+
+    im = Image.open(input_file_name)
+
+    new_width = 1000
+    pixelSize = int(new_width / int(count))
+    new_height = int(new_width * im.size[1] / im.size[0])
+    im = im.resize((new_width, new_height), Image.NEAREST)
+
+    d = DMC()
+    dmc_spaced = [[d.get_dmc_rgb_triple(im.getpixel((x, y))) for x in range(0, im.size[0], pixelSize)] for y in
+                  range(0, im.size[1], pixelSize)]
+
+    dmc_image = Image.new('RGB', (len(dmc_spaced[0]), len(dmc_spaced)))
+    dmc_image.putdata([value for row in dmc_spaced for value in row])
+
+    dmc_image = dmc_image.convert('P', palette=Image.ADAPTIVE, colors=num_colours)
+    x_count = dmc_image.size[0]
+    y_count = dmc_image.size[1]
+    svg_pattern = [[dmc_image.getpixel((x, y)) for x in range(x_count)] for y in range(y_count)]
+
+    palette = dmc_image.getpalette()
+    svg_palette = [d.get_colour_code_corrected((palette[i * 3], palette[i * 3 + 1], palette[i * 3 + 2])) for i in
+                   range(num_colours)]
+
+    if True:
+        for x in range(0, x_count):
+            for y in range(0, y_count):
+                gen = get_neighbours([y, x], svg_pattern)
+                neighbours = []
+                for n in gen:
+                    neighbours += [n]
+                if svg_pattern[y][x] not in neighbours:
+                    mode = max(neighbours, key=neighbours.count)
+                    svg_pattern[y][x] = mode
+
+    svg_cell_size = 10
+    width = x_count * svg_cell_size
+    height = y_count * svg_cell_size
+    col_sym.prep_for_drawing(width, height)
+    col_sym.mid_arrows(svg_cell_size, width, height)
+    blw_nsy.prep_for_drawing(width, height)
+    blw_nsy.mid_arrows(svg_cell_size, width, height)
+    col_nsy.prep_for_drawing(width, height)
+    x = y = svg_cell_size  # to allow drawing of midpoint arrows
+    for row in svg_pattern:
+        for colour_index in row:
+            col_sym.add_rect(svg_palette, colour_index, x, y, svg_cell_size)
+            blw_nsy.add_rect(svg_palette, colour_index, x, y, svg_cell_size)
+            col_nsy.add_rect(svg_palette, colour_index, x, y, svg_cell_size)
+            x += svg_cell_size
+        y += svg_cell_size
+        x = svg_cell_size
+    blw_nsy.major_gridlines(svg_cell_size, width, height)
+    col_sym.major_gridlines(svg_cell_size, width, height)
+
+    size = 40
+    key.prep_for_drawing(size * 13, size * len(svg_palette))
+    x = y = 0
+    for i in range(len(svg_palette)):
+        key.add_key_colour(x, y, size, i, svg_palette[i])
+        y += size
+
+    col_sym.save('col_sym.svg')
+    blw_nsy.save('blw_sym.svg')
+    col_nsy.save('col_nsy.svg')
+    key.save('key.svg')
+
+if __name__ == "__main__":
+    main()
     
-# a
-
-if(len(sys.argv)<3):
-    print("function requires an input filename, number of colours, stitch count and mode")
-    sys.exit(0)
-
-input_file_name = sys.argv[1]       # input file name, has to be a jpg
-num_colours = int(sys.argv[2])      # number of colours to use in the pattern
-count = int(sys.argv[3])            # stitch count, number of stitches in x axis
-
-# black_white, minor, symbols    
     
-col_sym = SVG(False, True, True)
-blw_nsy = SVG(True, True, True)
-col_nsy = SVG(False, False, False)
-key = SVG(False, True, True)
-
-# b
-
-im = Image.open(input_file_name)
-
-# c
-
-new_width  = 1000
-pixelSize = int(new_width / int(count))
-new_height = int(new_width * im.size[1] / im.size[0])
-im = im.resize((new_width, new_height), Image.NEAREST)
-
-# 1, 2
-
-d = DMC()
-dmc_spaced = [[d.get_dmc_rgb_triple(im.getpixel((x, y))) for x in range(0, im.size[0], pixelSize)] for y in range(0, im.size[1], pixelSize)]
-
-# 3
-
-dmc_image = Image.new('RGB', (len(dmc_spaced[0]), len(dmc_spaced))) #h, w
-dmc_image.putdata([value for row in dmc_spaced for value in row])
-
-# 4, 5
-
-dmc_image = dmc_image.convert('P', palette=Image.ADAPTIVE, colors = num_colours)
-x_count = dmc_image.size[0]
-y_count = dmc_image.size[1]
-svg_pattern = [[dmc_image.getpixel((x, y)) for x in range(x_count)] for y in range(y_count)]
-
-# 6
-
-palette = dmc_image.getpalette()
-svg_palette = [d.get_colour_code_corrected((palette[i * 3], palette[i * 3 + 1], palette[i * 3 + 2])) for i in range(num_colours)]
-
-# 7
-
-if True:
-    for x in range(0, x_count):
-        for y in range(0, y_count):
-            gen = get_neighbours([y, x], svg_pattern)
-            neighbours = []
-            for n in gen:
-                neighbours += [n]
-            if svg_pattern[y][x] not in neighbours:
-                mode = max(neighbours, key=neighbours.count)
-                svg_pattern[y][x] = mode
-
-# 8
-
-svg_cell_size = 10
-width = x_count * svg_cell_size
-height = y_count * svg_cell_size
-col_sym.prep_for_drawing(width, height)
-col_sym.mid_arrows(svg_cell_size, width, height)
-blw_nsy.prep_for_drawing(width, height)
-blw_nsy.mid_arrows(svg_cell_size, width, height)
-col_nsy.prep_for_drawing(width, height)
-x = y = svg_cell_size # to allow drawing of midpoint arrows
-for row in svg_pattern:
-    for colour_index in row:
-        col_sym.add_rect(svg_palette, colour_index, x, y, svg_cell_size)
-        blw_nsy.add_rect(svg_palette, colour_index, x, y, svg_cell_size)
-        col_nsy.add_rect(svg_palette, colour_index, x, y, svg_cell_size)
-        x += svg_cell_size
-    y += svg_cell_size
-    x = svg_cell_size
-blw_nsy.major_gridlines(svg_cell_size, width, height)
-col_sym.major_gridlines(svg_cell_size, width, height)
-
-# 9
-
-size = 40
-key.prep_for_drawing(size * 13, size * len(svg_palette))
-x = y = 0
-for i in range(len(svg_palette)):
-    key.add_key_colour(x, y, size, i, svg_palette[i])
-    y += size
-
-col_sym.save('col_sym.svg')
-blw_nsy.save('blw_sym.svg')
-col_nsy.save('col_nsy.svg')
-key.save('key.svg')
-
